12fada4505bd9c16ee78713ead3b61149e7214ee
Student <example@example.com>
2018-08-30 11:22:20 -0700

    add README content

--- dating-site/js/scripts.js
+++ dating-site/js/scripts.js
@@ -85,9 +85,9 @@ array.forEach(function(person, index){
     var allProfiles = [];
     var person = {};
     function profileGen() {
-      var firstNameMale =["glod", "snurr", "hlurgh", "hggghlhughl", "ooooomp", "pfleeep"]
-      var firstNameFemale = ["hlork", "glaaaar", "gneeeeed", "sneee", "pflurg", "shannon"]
-      var lastName =["matrix", "kruger", "owens", "rottmayer", "fries", "kaminsky", "quaid", "hauser", "vogel", "kimble"]
+      var firstNameMale =["Glod", "Snurr", "Hlurgh", "Ooooomp", "Fleeep", "Michael", "Christopher", "Matthew", "Joshua", "David", "Andrew", "Daniel", "Justin", "James", "Robert", "John", "Joseph", "Ryan", "Nicolas", "William", "Brandon", "Anthony", "Jonathan", "Kevin", "Kyle", "Jacob", "Jason"]
+      var firstNameFemale = ["Hlork", "Glaaaar", "Gneeeeed", "Sneee", "Pflurg", "Shannon", "Jessica", "Ashley", "Amanda", "Brittany", "Jennifer", "Sarah", "Stephanie", "Samantha", "Elizabeth", "Lauren", "Nicole", "Megan", "Heather", "Amber", "Melissa", "Tiffany", "Danielle", "Emily"]
+      var lastName =["matrix", "Kruger", "Owens", "Rottmayer", "Fries", "Kaminsky", "Quaid", "Hauser", "Vogel", "Kimble", "Grace", "Haller", "Murphy", "Shea", "Gosser", "Smith", "Black", "White", "Green", "Pink", "Burger", "Ketchip", "Lettuce", "Whereman", "McDonald", "Wendy", "King", "Arby"]
       var phone ="0000000"
       var age = [25, 44]
       var height = ["short", "tall"]
@@ -98,8 +98,8 @@ array.forEach(function(person, index){
       var relationship = ["casual", "marriage"]
       var personality = [["a"], ["b"], ["c"], ["d"]]
       var bio = ["I just love the B52’s and a good Tequila Sunrise or Pina Colada.  My idea of a first date would be dinner and clubbing tidz ll dawn.  Hope you have a Mullet and a Rolex.", "If your looking for a guy that loves to ski that’s me!  I spend a lot of time at my Condo in Vermont whenever I can get away from the brokerage. I just got a new espresso machine so my idea of a great first date would be for the two of us to learn how it works while listening to Michael Jackson on my new cassette system.", "I’m into sports cars.  I drive a new red turbo Porsche and love clubbing all the hot spots in the City.  Hoping to make a real connection with a person that feels the “need for speed” and wants a real “Top Gun” sort."]
-      var imageMale = ["img/man1.jpg", "img/man2.jpg", "img/man3.jpg", "img/man4.jpg", "img/man5.jpg"]
-      var imageFemale = ["img/woman1.jpg", "img/woman2.jpg", "img/woman3.jpg", "img/woman4.jpg", "img/woman5.jpg"]
+      var imageMale = ["img/man1.jpg", "img/man2.jpg", "img/man3.jpg", "img/man4.jpg", "img/man5.jpg", "img/man6.jpg", "img/man7.jpg", "img/man8.jpg", "img/man9.jpg", "img/man10.jpg", "img/man11.jpg", "img/man12.jpg", "img/man13.jpg", "img/man14.jpg", "img/man15.jpg", "img/man16.jpg", "img/man17.jpg", "img/man18.jpg", "img/man19.jpg", "img/man20.jpg", "img/men21.jpg"]
+      var imageFemale = ["img/woman1.jpg", "img/woman2.jpg", "img/woman3.jpg", "img/woman4.jpg", "img/woman5.jpg", "img/woman6.jpg", "img/woman7.jpg", "img/woman8.jpg", "img/woman9.jpg", "img/woman10.jpg","img/woman11.jpg", "img/woman12.jpg", "img/woman13.jpg", "img/woman14.jpg", "img/woman15.jpg", "img/woman16.jpg", "img/woman17.jpg", "img/woman18.jpg", "img/woman19.jpg", "img/woman20.jpg"]
       for (var h = 0; h < age.length ; h++){
         for (var i = 0; i < height.length ; i++) {
           for (var j = 0; j <gender.length ; j++) {
@@ -108,11 +108,11 @@ array.forEach(function(person, index){
                 for (var m = 0; m <  heightPref.length; m++){
                   for (var n = 0; n < relationship.length; n++){
                     for (var o = 0; o < personality.length; o++) {
-                      if (j == 1){
+                      if (j == 0){
 
-                        allProfiles.push(new Profile(firstNameFemale[randInt(0, firstNameFemale.length-1)], lastName[randInt(0, lastName.length-1)], phone, age[h], height[i], gender[j], orientation[k], agePref[l], heightPref[m], relationship[n], personality[o], bio[randInt(0, bio.length-1)], imageFemale[randInt(0, imageFemale.length-1)]))
+                        allProfiles.push(new Profile(firstNameFemale[randInt(0, firstNameFemale.length-1)], lastName[randInt(0, lastName.length-1)], randInt(100, 999) + "-" + randInt(1000, 9999), age[h], height[i], gender[j], orientation[k], agePref[l], heightPref[m], relationship[n], personality[o], bio[randInt(0, bio.length-1)], imageFemale[randInt(0, imageFemale.length-1)]))
                       } else {
-                        allProfiles.push(new Profile(firstNameMale[randInt(0, firstNameMale.length-1)], lastName[randInt(0, lastName.length-1)], phone, age[h], height[i], gender[j], orientation[k], agePref[l], heightPref[m], relationship[n], personality[o], bio[randInt(0, bio.length-1)], imageMale[randInt(0, imageFemale.length-1)]))
+                        allProfiles.push(new Profile(firstNameMale[randInt(0, firstNameMale.length-1)], lastName[randInt(0, lastName.length-1)], randInt(100, 999) + "-" + randInt(1000, 9999), age[h], height[i], gender[j], orientation[k], agePref[l], heightPref[m], relationship[n], personality[o], bio[randInt(0, bio.length-1)], imageMale[randInt(0, imageFemale.length-1)]))
                       }
                     }
                   }
@@ -161,7 +161,7 @@ console.log(allProfiles);
         $("#personList").append( '<div class="card col-md-4" style="width: 18rem;">' +
                 '<img class="card-img-top match" src="' + match.image + '" alt="Card image cap">' +
                 '<div class="card-body">' +
-                  '<h5 class="card-title"> Name: ' + match.firstName + '</h5>' +
+                  '<h5 class="card-title"> Name: ' + match.firstName + " " + match.lastName + '</h5>' +
                   '<p class="card-text">Bio: ' + match.bio + '</p>' +
                 '</div>' +
                 '<ul class="list-group list-group-flush">' +
